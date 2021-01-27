e72f781fb078ee4aeb5ec5f4470a0faa78d4d02e
Philip Larsen <Kjedtoft@gmail.com>
2021-01-27 16:18:10 +0100

    Starwars program with LINQ
    Program where I use LINQ

---
+++ Starwars/Starwars/NoRotationComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starwars
+{
+    class NoRotationComparer : IEqualityComparer<Planet>
+    {
+
+        public bool Equals(Planet x, Planet y)
+        {
+            if (int.Equals(x.RotationPeriod, y.RotationPeriod))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(Planet obj)
+        {
+            return obj.RotationPeriod.GetHashCode();
+        }
+    }
+}

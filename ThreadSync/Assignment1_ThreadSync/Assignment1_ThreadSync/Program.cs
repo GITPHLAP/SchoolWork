c03c37e666fd5d29c435ac853e48bb9bee1fda2e
Philip K. Larsen <kjedtoft@gmail.com>
2021-03-01 16:24:27 +0100

    threading and synkronisering

---
+++ ThreadSync/Assignment1_ThreadSync/Assignment1_ThreadSync/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Assignment1_ThreadSync
+{
+    class Program
+    {
+        static int sum;
+
+        static object locks = new object();
+
+        static void Main(string[] args)
+        {
+            //Create the 2 threads
+            Thread t1 = new Thread(Up);
+
+            Thread t2 = new Thread(Down);
+
+            t1.Start();
+            t2.Start();
+
+            while (true)
+            {
+                //Write it to console 
+                Monitor.Enter(locks);
+                Console.WriteLine(sum);
+                Monitor.Exit(locks);
+            }
+
+        }
+
+        static void Up()
+        {
+            while (true)
+            {
+                //Create the lock and add 2 to sum
+                Monitor.Enter(locks);
+                sum = sum + 2;
+                Monitor.Exit(locks);
+                Thread.Sleep(1000);
+            }
+            
+        }
+        static void Down()
+        {
+            while (true)
+            {
+                Monitor.Enter(locks);
+                sum--;
+                Monitor.Exit(locks);
+                Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
---
+++ ThreadSync/Assignment2+3_Threadingsync/Assignment2+3_Threadingsync/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Assignment2_3_Threadingsync
+{
+    class Program
+    {
+        static int currentchar;
+
+        static object locks = new object();
+        static void Main(string[] args)
+        {
+            //Create the threads
+            Thread starT = new Thread(WriteStars);
+            Thread tagT = new Thread(WriteTags);
+
+            starT.Start();
+            tagT.Start();
+
+
+            Console.ReadLine();
+
+        }
+
+
+        static void WriteStars()
+        {
+            while (true)
+            {
+                //enter the lock
+                Monitor.Enter(locks);
+                Thread.Sleep(1000);
+                string starstring = "";
+                for (int i = 0; i < 60; i++)
+                {
+                    starstring = starstring + "*";
+                }
+
+                currentchar = currentchar + starstring.Length;
+
+                Console.WriteLine("\n{0} {1}", starstring, currentchar);
+
+                //exit the lock 
+                Monitor.Exit(locks);
+            }
+            
+            
+        }
+
+        static void WriteTags()
+        {
+            while (true)
+            {
+                //enter the lock
+                Monitor.Enter(locks);
+
+                //thread sleep
+                Thread.Sleep(1000);
+
+                //local string to write to console
+                string tagstring = "";
+                //add # to string 60 times
+                for (int i = 0; i < 60; i++)
+                {
+                    tagstring = tagstring + "#";
+                }
+
+                //Take the length and add it 
+                currentchar = currentchar + tagstring.Length;
+
+                Console.WriteLine("\n{0} {1}", tagstring, currentchar);
+
+                //exit the lock 
+                Monitor.Exit(locks);
+            }
+            
+
+        }
+    }
+}

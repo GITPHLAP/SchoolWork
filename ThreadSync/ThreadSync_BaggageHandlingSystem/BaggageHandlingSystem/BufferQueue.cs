8b3608623ae05db78b16fcb709161067c32de747
Philip K. Larsen <kjedtoft@gmail.com>
2021-03-02 16:37:13 +0100

    Bagage sorterings system begin

---
+++ ThreadSync/ThreadSync_BaggageHandlingSystem/BaggageHandlingSystem/BufferQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaggageHandlingSystem
+{
+    class BufferQueue<T> : Queue<T>
+    {
+        //Set a limit om the queue
+        public int BufferSize { get; set; }
+
+        public BufferQueue(int maxsize) : base(maxsize)
+        {
+            BufferSize = maxsize;
+        }
+
+        //make a new Enqueue Method 
+        public new void Enqueue(T item)
+        {
+            // if count smaller then maxsize then enqueue else do nothing
+            if (this.Count < BufferSize)
+            {
+                base.Enqueue(item);
+            }
+
+        }
+
+        public bool IsLimitReached()
+        {
+            return this.Count >= this.BufferSize ? true : false;
+        }
+    }
+}

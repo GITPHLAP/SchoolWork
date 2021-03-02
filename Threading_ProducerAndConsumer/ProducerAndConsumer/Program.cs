8b078dddbbdf30b0a064858c37e640d02b78cd68
Philip K. Larsen <kjedtoft@gmail.com>
2021-03-02 10:56:22 +0100

    ProducerAndConsumer

---
+++ Threading_ProducerAndConsumer/ProducerAndConsumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProducerAndConsumer
+{
+    class Program
+    {
+         int space = ;
+        static void Main(string[] args)
+        {
+
+            Semaphore test = BUFFER_SIZE; 
+        }
+    }
+}
